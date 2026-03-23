e7248e8fce959b7836112d18c8816b3360838a10
Inácio Filho <inaciofilho.lima@gmail.com>
2026-03-23 19:35:10 -0300

    Ajusta número de workers para o servidor de produção no entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,7 +6,7 @@ alembic upgrade head
 
 echo "Starting server..."
 if [ "$ENV" = "production" ]; then
-    exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers 1
+    exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers 2
 else
     exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
 fi
